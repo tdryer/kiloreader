e418947bd95cdbb5cf69400353a2c5b69fcb3d82
Tom Dryer <tomdryer.com@gmail.com>
2013-05-27 20:15:15 -0700

    Port to AngularJS, custom infinite scroll code

--- script.js
+++ script.js
@@ -1,49 +1,45 @@
-function appendPost(i, post_json) {
-    var post_template = '\
-        <div class="post">\
-            <a class="post-title" href="#">{title}</a>\
-            <p>{content}</p>\
-        </div>\
-    ';
-    console.log("append post " + post_json + " with title " + post_json["title"]);
-    $('#content').append(post_template.replace("{title}", post_json["title"]).replace("{content}", post_json["content"])
-    );
-}
-
-function setFeedList(feeds) {
-    var list = $('#sidebar ul');
-    $.each(feeds, function(i, feed) {
-        console.log("add feed " + feed);
-        var handler = 'javascript:selectFeed(\'' + feed + '\')';
-        $("<li>", {}).append($("<a>", {'href': handler, 'text': feed, 'id': feed})).appendTo(list);
-    });
-
-}
-
-function selectFeed(feed) {
-    console.log("Selected feed: " + feed);
-}
-
-$( document ).ready(function() {
-
-    setFeedList(["The Verge", "XKCD", "Planet Ubuntu"]);
-
-    $('#content').infiniteScroll({
-        threshold: 50,
-        onEnd: function() {
-            console.log('No more results!');
-        },
-        onBottom: function(callback) {
-            console.log('At the end of the page. Loading more!');
-
-            $.getJSON("test_data.json", function (data) {
-                $.each(data["posts"], appendPost);
-            });
-
-            // (load results & update views)
-            var moreResults = true;
-
-            callback(moreResults);
+angular.module('scroll', []).directive('whenScrolled', function() {
+    return function(scope, elm, attr) {
+        var raw = elm[0];
+        // TODO: stop when there's no more content
+        // TODO: make sure enough content has loaded for initial scroll bar to appear
+        var checkScroll = function() {
+            if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                scope.$apply(attr.whenScrolled);
+            }
         }
-    });
+        elm.bind('scroll', checkScroll);
+        $(window).bind('resize', checkScroll);
+    };
 });
+
+function MyController($scope, $http) {
+    $scope.title = "TReader";
+
+    $scope.feeds = {
+        1: "The Verge",
+        2: "XKCD",
+        3: "Planet Ubuntu",
+    };
+
+    $scope.posts = [];
+
+    $scope.select_feed = function(feed_id) {
+        console.log("Selected feed: " + $scope.feeds[feed_id]);
+    };
+
+    $scope.add_post = function(i, post) {
+        console.log("Added post " + post["title"]);
+        $scope.posts.push(post);
+    };
+
+    $scope.add_more_posts = function() {
+        console.log('At the end of the page. Loading more!');
+        $http.get("test_data.json").success(function (data, status) {
+            $.each(data["posts"], $scope.add_post);
+        });
+    };
+
+    $scope.add_more_posts();
+}
+
