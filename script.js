d4a022770bc707b4a04cbc96fdf67aea931de15d
Tom Dryer <tomdryer.com@gmail.com>
2013-06-21 20:44:37 -0700

    Select entries during scrolling

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-angular.module('scroll', []).directive('whenScrolled', function() {
+var module = angular.module('scroll', [])
+
+module.directive('whenScrolled', function() {
     return function(scope, elm, attr) {
         var raw = elm[0];
         // TODO: stop when there's no more content
@@ -13,6 +15,32 @@ angular.module('scroll', []).directive('whenScrolled', function() {
     };
 });
 
+module.directive('onScrollCurrent', function() {
+    return function(scope, elm, attr) {
+        var get_current = function() {
+            var raw = elm[0];
+            var scroll_pos = raw.scrollTop;
+            var entries = $('.entry');
+            // min distance between top of scroll area and entry top before the entry is selected
+            var BUFFER = 50;
+            for (var i = 0; i < entries.length; i++) {
+                var entry = $(entries[i]);
+                var entry_top = entry[0].offsetTop - BUFFER;
+                var entry_height = entry.height();
+                if (scroll_pos >= entry_top && scroll_pos < entry_top + entry_height) {
+                    scope.$apply(function() {
+                        scope.selected_entry = i;
+                    });
+
+                }
+
+            }
+        };
+        elm.bind('scroll', get_current);
+    };
+
+});
+
 function MyController($scope, $http) {
     $scope.title = "TReader";
 
@@ -22,11 +50,15 @@ function MyController($scope, $http) {
 
     $scope.selected_feed = -1;
 
+    $scope.selected_entry = -1;
+
     $scope.select_feed = function(feed_id) {
         if (feed_id !== $scope.selected_feed) {
             console.log("Selected feed: " + $scope.feeds[feed_id].title);
             $scope.selected_feed = feed_id;
             $scope.entries = [];
+            // start with no entry selected (only sorta works)
+            $scope.selected_entry = -1;
             $scope.load_entries();
         }
     };
